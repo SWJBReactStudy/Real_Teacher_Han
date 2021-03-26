23c71d205e2299c5b9263a89d31b5ad3b89e6eb9
ArKc1009 <arkc1009@gmail.com>
2021-03-26 23:56:41 +0900

    feat: 조건부렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
+import Render from "./Render";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [isRender, setIsRender] = useState(true);
 
   return (
     <div>
       <div>{count}</div>
       <button onClick={() => setCount(count + 1)}>+</button>
       <button onClick={() => setCount(count - 1)}>-</button>
+      
+      <Render count={[count, setCount]} />
     </div>
   );
 };
---
+++ src/Render.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Render = (props) => {
+  let asd = <div>asdasfaf</div>;
+  
+  return <div>{props.count[0] ? "asdsad" : "bgggs"}</div>;
+};
+
+export default Render;
