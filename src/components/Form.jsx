740fabb084eb5f06c1f092fb8569a33041cc6495
ArKc1009 <arkc1009@gmail.com>
2021-03-27 22:46:03 +0900

    input 태그 공부

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [student, setStudent] = useState({
+    name: "asfas",
+    hakbun: "safafa",
+  });
+
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+    setStudent({
+      ...student,
+      [name]: value,
+    });
+  };
+
   return (
     <div>
-      <input type="text" />
+      <label>이름</label>
+      <input
+        type="text"
+        onChange={onChangeInput}
+        value={student.name}
+        name="name"
+      />
+
+      <br />
+
+      <label>학번</label>
+      <input
+        type="text"
+        onChange={onChangeInput}
+        value={student.hakbun}
+        name="hakbun"
+      />
+
+      <div>
+        <p>이름 : </p>
+        <p>학번 : </p>
+      </div>
     </div>
   );
 };
