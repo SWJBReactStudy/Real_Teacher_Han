4777fe2f718ce9d736d93f07dc407387fbe5c63d
ArKc1009 <arkc1009@gmail.com>
2021-04-04 19:09:10 +0900

    teach: styled-components

---
+++ src/components/Blank.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Blank = styled.div`
+  width: ${(props) => props.width + "px"};
+  height: ${(props) => props.height + "px"};
+`;
+
+export default Blank;
